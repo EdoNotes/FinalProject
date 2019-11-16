2c04b9b41ed2f9f0614ba08244e9c4f960af4e7b
Edo Notes <edoono24@gmail.com>
2019-11-16 16:35:45 +0200

    merged project

---
+++ app/src/main/java/com/example/finalproject/BlurringService.java
@@ -0,0 +1,116 @@
+package com.example.finalproject;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.graphics.Point;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import java.util.Vector;
+
+public class BlurringService extends Service
+{
+    IBinder mBinder = new LocalBinder();
+    private static final String TAG = "BlurringService";
+    private WindowManager windowManager;
+    private ConstraintLayout blurringView;
+    private Vector<ConstraintLayout> blurringViews=new Vector<ConstraintLayout>();
+    private Vector<ConstraintLayout> alreadyblurredViews=new Vector<ConstraintLayout>();
+    private LinearLayout bubbleLayout;
+    private Point szWindow = new Point(0,0);
+    private LayoutInflater layoutInflater;
+    WindowManager.LayoutParams params;
+    /**
+     *Todo later
+     */
+    public BlurringService()
+    {
+    }
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        Log.d(TAG,"OnCreate()");
+        handleStart();
+    }
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        if (blurringView!= null)
+            windowManager.removeView(blurringView);
+        if (bubbleLayout != null)
+            windowManager.removeView(bubbleLayout);
+    }
+    private void handleStart(){
+        windowManager=(WindowManager) getSystemService(WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getSize(szWindow);
+        layoutInflater=(LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        //create blurring view
+        blurringView = (ConstraintLayout) layoutInflater.inflate(R.layout.layout_bubble_head, null);
+        params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                PixelFormat.TRANSLUCENT);
+    }
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        return mBinder;
+    }
+    public void blur(Vector<BlurData> dataVec)
+    {
+        int i=0;
+        if(dataVec.size()==0)
+        {
+            clean();
+            return;
+        }
+        clean();//clean previous views first
+        for(BlurData bd:dataVec)
+        {
+            ConstraintLayout curr;
+            curr=(ConstraintLayout) layoutInflater.inflate(R.layout.layout_bubble_head, null);
+            blurringViews.add(curr);
+        }
+        for(ConstraintLayout view:blurringViews)
+        {
+            BlurData curr=dataVec.get(i);
+            params.x=curr.getX();
+            params.y=curr.getY();
+            params.height=curr.getHeight();
+            params.width=curr.getWidth();
+            view.setLayoutParams(params);
+            windowManager.addView(view,params);
+            alreadyblurredViews.add(view);
+            i++;
+        }
+        blurringViews.clear();//make the vector empty for next blurring
+    }//blur
+    public void clean()
+    {
+        for(ConstraintLayout view:alreadyblurredViews)
+        {
+            windowManager.removeViewImmediate(view);
+        }
+        alreadyblurredViews.clear();
+    }
+    public class LocalBinder extends Binder
+    {
+        public BlurringService getServerInstance()
+        {
+            return BlurringService.this;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -0,0 +1,245 @@
+package com.example.finalproject;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+//import android.support.design.widget.Snackbar;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.app.Activity;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.Vector;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import java.nio.ByteBuffer;
+
+
+public class MainActivity extends Activity {
+
+    public static final int CODE_DRAW_OVER_OTHER_APP_PERMISSION = 2084;
+    boolean mBounded;
+    BlurringService mServer;
+    private Button blurButton;
+    private static final String TAG = "MainActivity";
+    private CaptureScreen mCapture = null;
+    private volatile int imageAvailable = 0;
+
+    private FrameClassifier classifierObj = null;
+    private Runnable runnableObj = null;
+
+    public static final int defineDelayMilli = 200;
+    public static final int definedRatio = 300;
+    public static final int definedDensity = 420;
+    public static final float definedThreshold = (float)0.55;
+
+    private final Lock lock = new ReentrantLock(true);
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Intent intent = new Intent(this, BlurringService.class);
+        startService(intent);
+        bindService(intent,mConnection,BIND_AUTO_CREATE);
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        blurButton=(Button)findViewById(R.id.blur_Btn);
+        blurButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                if(isLayoutOverlayPermissionGranted(MainActivity.this))
+                {
+                    Vector<BlurData> dataVector=new Vector<BlurData>();
+                    dataVector.add(new BlurData(200,500,200,200));
+                    dataVector.add(new BlurData(400,0,500,200));
+                    mServer.blur(dataVector);
+                }
+                else
+                {
+                    grantLayoutOverlayPermission(MainActivity.this);
+                }
+            }
+        });
+        // Init gui
+        final Button StartBtn = (Button) findViewById(R.id.StartBtn);
+        final Button StopBtn = (Button) findViewById(R.id.StopBtn);
+        //final ImageView imgTrump = (ImageView) findViewById(R.id.imageView2);
+
+        StartBtn.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Snackbar.make(v, "Capture screen started..", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                if(mCapture == null) {
+                    mCapture = new CaptureScreen(MainActivity.this , defineDelayMilli, definedRatio, definedDensity);
+                    mCapture.StartCaputre();
+
+                    classifierObj = new FrameClassifier(MainActivity.this , definedRatio);
+
+                    runnableObj = new Runnable() {
+                        public void run() {
+
+                            long startTimeMilli = -1;
+                            IntObj frameCounter = new IntObj();
+                            ByteBuffer buff;
+
+                            while(null != mCapture) {
+                                lock.lock();
+                                try {
+                                    startTimeMilli = System.currentTimeMillis();
+
+                                    buff = mCapture.GetLatestFrame(frameCounter);
+
+                                    if (null != buff) {
+                                        ArrayList<int[]> results;
+                                        if (null != classifierObj) {
+                                            classifierObj.PredictFrame(buff, definedThreshold);
+                                            results = classifierObj.GetCoordinates();
+
+                                            results = null;
+                                            // Add draw functions
+                                        } else
+                                            classifierObj = new FrameClassifier(MainActivity.this, definedRatio);
+                                    }
+
+                                } catch (Exception e) {
+                                    Log.i(TAG, e.toString());
+                                } finally {
+                                    lock.unlock();
+                                }
+
+                                while ((System.currentTimeMillis() - startTimeMilli) < defineDelayMilli) {
+                                    try {
+                                        Thread.sleep(1);
+                                    } catch (Exception e) {
+                                        Log.i(TAG, "Failed to Sleep in Background thread");
+                                    }
+                                }
+                            }
+
+                        }
+                    };
+
+                    new Thread(runnableObj).start();
+                }
+
+            }
+        });
+
+
+        StopBtn.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Snackbar.make(v, "Capture screen stopped..", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                //((TextView)findViewById(R.id.textView)).setText("Running...");
+
+
+                if(mCapture != null) {
+                    lock.lock();
+                    try {
+                        mCapture.StopCaputre();
+                        mCapture = null;
+                    } catch (Exception e) {
+                        // handle the exception
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if(mCapture != null) {
+            mCapture.StopCaputre();
+            mCapture = null;
+        }
+        Intent intent = new Intent(this, BlurringService.class);
+        if (mBounded) {
+            mServer.onUnbind(intent);
+            mBounded = false;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == CaptureScreen.PERMISSION_CODE) {
+            mCapture.CaptureScreenActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    public class IntObj {
+        public long value;
+    }
+    ServiceConnection mConnection = new ServiceConnection()
+    {
+        @Override
+        public void onServiceDisconnected(ComponentName name)
+        {
+            mBounded = false;
+            mServer = null;
+        }
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service)
+        {
+            mBounded = true;
+            BlurringService.LocalBinder mLocalBinder = (BlurringService.LocalBinder)service;
+            mServer = mLocalBinder.getServerInstance();
+        }
+    };
+    private boolean isLayoutOverlayPermissionGranted(Activity activity)
+    {
+        Log.v(TAG,"Granting Layout Overlay Permission..");
+        if(Build.VERSION.SDK_INT>=23 && !Settings.canDrawOverlays(activity))
+        {
+            Log.v(TAG,"Permission is denied");
+            return false;
+        }
+        else
+        {
+            Log.v(TAG,"Permission is granted");
+            return true;
+        }
+    }//isLayoutOverlayPermissionGranted
+    private void grantLayoutOverlayPermission(Activity activity)
+    {
+        if(Build.VERSION.SDK_INT>=23 &&!Settings.canDrawOverlays(activity))
+        {
+            Intent intent=new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + activity.getPackageName()));
+            activity.startActivityForResult(intent,CODE_DRAW_OVER_OTHER_APP_PERMISSION);
+        }
+    }
+}
+
+
+
+
+
+
